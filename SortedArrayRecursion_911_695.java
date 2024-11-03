826fce875dae88bcd2fcd80261e4f73792919cb4
vishalraghav0290 <vishalraghavmac@gmail.com>
2024-11-03 19:34:50 +0530

    Add solution for problem SortedArrayRecursion_911

---
+++ SortedArrayRecursion_911_695.java
@@ -0,0 +1,24 @@
+public class SortedArrayRecursion{
+
+    public static boolean isSorted(int array[], int index){
+        if(index==array.length-1){
+            return true;
+        }
+         if(array[index]>array[index+1]){
+            return false;
+         }
+         else
+          return isSorted(array, index+1);
+         
+
+
+    }
+
+    public static void main(String args[]){
+        int array[] ={ 1,6,3,4,9};
+        int index=0;
+        boolean ans =isSorted(array, index);
+        System.out.println(ans );
+
+    }
+}
