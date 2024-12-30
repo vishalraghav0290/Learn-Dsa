b0df96795fedf34cefc6ac500f68d85a9ecd3e44
vishalraghav0290 <vishalraghavmac@gmail.com>
2024-12-30 16:56:05 +0530

    Add solution for problem Array_462

---
+++ Array_462_786.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Array{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int math[] = new int[50];
+         math[1]=45;
+        math[2]=33;
+        math[3]=78;
+        int sum =(math[1]+math[2]+math[3])/3;
+        System.out.println(sum);
+    }
+}
